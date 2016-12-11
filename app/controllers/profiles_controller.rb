85b5f889751b263ca6f684b1eaf8ef7e68ae75ee
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-12-11 03:04:13 +0000

    add profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,27 +1,43 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_profile, only: [:show]
+
   def index
     @profiles = Profile.all
   end
 
-  def create
+  def new
+    @profile = Profile.new
+  end
 
+  def create
+    @profile = current_user.profile.create(profile_params)
+    if @profile.valid?
+      redirect_to profile_path(@profile)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
     @profile = Profile.find(params[:id])
   end
 
-  def update
-
-  end
-
-  def destroy
-
-  end
-
   private
-  def show_params
-    @profile = Profile.find(params[:id])
+
+  def require_authorized_for_current_profile
+    if current_profile.user != current_user
+      render text: "Unauthorized", status: :unauthorized
+    end
   end
 
+  helper_method :current_profile
+
+  def current_profile
+    @current_profile ||= Profile.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :bio, :location)
+  end
 end
